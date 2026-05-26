41d0333d870e2a865b51a4ea2ad6044942d59f2d
comkhj82 <comkhj82@gmail.com>
2026-05-26 17:48:27 +0900

    260526

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+let modal_img = document.querySelector('.modal img_section');
+let modal_close = document.querySelector('.modal .close');
+let modal_open = document.querySelector('.modal .open');
+
+modal_img.onclick = () => {
+    for (let i = 0; i < modal_img.length; i++) {
+    popup.style.display = 'flex';
+    }
+
+}
